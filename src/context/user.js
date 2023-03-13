c8fa2c05958a3ef9c974e4f4dffa2426102e1eb6
Lela Deslauriers <lela.deslauriers@gmail.com>
2023-03-12 21:06:08 -0700

    commit tree 6adef9453104cdf4663e38335fff769c3650cfea

---
+++ src/context/user.js
@@ -0,0 +1,19 @@
+// src/context/user.js
+import React, { useState } from "react";
+
+// create the context
+const UserContext = React.createContext();
+
+// create a provider component
+function UserProvider({ children }) {
+    const [user, setUser] = useState(null);
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+  }
+  // the value prop of the provider will be our context data
+  // this value will be available to child components of this provider
+
+export { UserContext, UserProvider };
